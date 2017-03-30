28473dc8ecc4680bd6874df7a6022603a30209c8
Marx JMoura <marx.jmoura@gmail.com>
2017-03-30 16:39:37 -0300

    Custom validation attribute

---
+++ src/Dyoub.App/Filters/ValidIfAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Dyoub.App.Filters
+{
+    public class ValidIfAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return (bool)value;
+        }
+    }
+}
