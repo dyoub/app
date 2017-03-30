e973f37ba0214b1f2b8b46e469bee29cead511b2
Marx JMoura <marx.jmoura@gmail.com>
2017-03-30 16:39:37 -0300

    Add authorization attribute

---
+++ src/Dyoub.App/Filters/AuthorizationAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+using Dyoub.App.Extensions;
+using Dyoub.App.Models.EntityModel;
+using Dyoub.App.Models.EntityModel.Account;
+using Dyoub.App.Infrastructure.Security;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Dyoub.App.Filters
+{
+    public class AuthorizationAttribute : AuthorizeAttribute
+    {
+        
+    }
+}
---
+++ src/Dyoub.Test/Filters/AuthorizationAttributeTest.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dyoub.Test.Filters
+{
+    [TestClass]
+    public class AuthorizationAttributeTest
+    {
+
+    }
+}
